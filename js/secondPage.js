36da4a6c575f2514996439a7161e922a5c74e908
JoshuaRamc <joshuaramcharan818@gmail.com>
2023-09-18 09:59:03 -0400

    Add second page features for The Office

---
+++ js/secondPage.js
@@ -0,0 +1,5 @@
+const secondQuote = document.body.querySelector(".secondTextPage");
+const setQuote = document.body.querySelector("#generate-quote");
+setQuote.addEventListener("click", () => {
+    secondQuote.textContent = "Would I rather be feared or loved? Easy. Both. I want people to be afraid of how much they love me."
+});
